2ffcc959ffbc6e91240386f3eb2e881784acace4
bernardtom <bernard.tom.pro@gmail.com>
2026-02-06 12:28:09 +0100

    check inputs and load config for qrcode class

---
+++ qrcode.py
@@ -0,0 +1,32 @@
+import numpy as np
+import json
+
+CONFIG_FILE = 'config.json'
+
+class QRCode:
+    def __init__(self,data_string:str,correction_level="Q"):
+        # Load config file
+        self.config = self.load_config(CONFIG_FILE)
+
+        # Check inputs types
+        if not isinstance(data_string,str):
+            raise TypeError('Invalid Type for error correction level parameter')
+        if not isinstance(correction_level,str):    
+            raise TypeError('Invalid Type for error correction level parameter')
+        
+        # Check inputs values
+        if not len(data_string) > 0:
+            raise ValueError('Empty Value for data_string parameter')
+        if not correction_level in self.encode_config['correction_levels']:           
+            raise ValueError('Invalide Value for error correction level parameter')
+        
+        self.data_string = data_string
+        self.correction_level = correction_level
+
+    def load_config(self,filename:str)->dict:
+        try:
+            with open(filename,'r',encoding='utf-8') as file:
+                config = json.load(file)
+            return config
+        except:
+            raise FileNotFoundError('config file not founded')
